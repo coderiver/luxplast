69afd3d9cb3bfd32574931b88956164c50d09437
Vadim Hraboviy <hraboviyvadim@gmail.com>
2014-12-04 20:12:22 +0530

    main page start

--- js/common.js
+++ js/common.js
@@ -1,8 +1,22 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+// slick
 
-	console.log($('body').html());
+	$('.js-banner').slick({
+		dots: true,
+		arrows: true,
+		adaptiveHeight: true
+	});
+
+// sticky header shadow
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 1) {
+			$('.header__in').addClass('is-sticky');
+		}
+		else{
+			$('.header__in').removeClass('is-sticky');
+		}
+	});	
+	
 });
