3b169445192644b47b4cc75164035a862af441fd
Vadim Hraboviy <hraboviyvadim@gmail.com>
2014-12-05 11:59:48 +0530

    index page

--- js/common.js
+++ js/common.js
@@ -10,13 +10,49 @@ head.ready(function() {
 
 // sticky header shadow
 
-	$(window).scroll(function(){
+	function headerScroll(){
 		if ($(window).scrollTop() > 1) {
 			$('.header__in').addClass('is-sticky');
 		}
 		else{
 			$('.header__in').removeClass('is-sticky');
 		}
-	});	
+	}
+	headerScroll();	
 	
+// menu toggle
+	$('.js-nav-btn').on('click', function(){
+		if ($('.js-menu').hasClass('is-open')) {
+			$('.js-menu').removeClass('is-open');
+		}
+		else{
+			$('.js-menu').addClass('is-open');
+			event.stopPropagation();
+		}
+		
+	});
+	$(document).on('click', function(event){
+		$('.js-menu').removeClass('is-open');
+	});
+	$('.js-menu').on('click', function(){
+		event.stopPropagation();
+	});
+
+	function menuWidth(){
+		var left = $('.js-nav-btn').offset().left;
+		width = (left + 46);
+		$('.js-menu').css('width', width);
+	};
+	menuWidth();
+
+// scroll
+	$(window).scroll(function(){
+		headerScroll();
+	});
+
+// resize
+	$(window).resize(function(){
+		menuWidth();
+	});
+
 });
